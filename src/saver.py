a19a107cd275c2369b710262883bc6fcb3bb6aa0
DuckRainbow <sillestyna@gmail.com>
2024-03-10 21:42:05 +0500

    save to json

---
+++ src/saver.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class Save(ABC):
+    @abstractmethod
+    def save(self, *args, **kwargs):
+        pass
+
+
+class SaveToJSON(Save):
+    def __init__(self, file_path, data_to_save):
+        self.data_to_save = data_to_save
+        self.file_path = file_path
+
+    def save(self):
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            f.write(self.data_to_save)
+
+    def show(self):
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            data_to_show = f.read(self.data_to_save)
+        return data_to_show
--- src/vacancy.py
+++ src/vacancy.py
@@ -1,11 +1,28 @@
 class Vacancy:
-    def __init__(self, title, url, salary, descripton, requirements):
+    vacancy_id = 0
+
+    def __init__(self, title, url, salary_from, salary_to, description, requirements):
         self.title = title
         self.url = url
-        self.salary = salary
-        self.descripton = descripton
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.description = description
         self.requirements = requirements
+        self.validate_salary()
+        Vacancy.vacancy_id += 1
+        self.vacancy_id = Vacancy.vacancy_id
 
+    def validate_salary(self):
+        if not self.salary_to:
+            self.salary_to = 0
+        if not self.salary_from:
+            self.salary_from = 0
 
-    def salary_dif(self, other):
-        pass
+    @property
+    def salary(self):
+        if self.salary_from < self.salary_to:
+            return self.salary_to
+        return self.salary_from
+
+    def __lt__(self, other):
+        return self.salary > other.salary
