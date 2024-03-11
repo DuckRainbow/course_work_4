9a6018b2764e8e187d5ba54ac8d861d55a12b977
DuckRainbow <sillestyna@gmail.com>
2024-03-11 21:18:47 +0500

    add tests

--- src/func.py
+++ src/func.py
@@ -10,9 +10,10 @@ def filter_city(vacancies_list, city):
 
 def filter_vacancies(city_vacancies, filter_words):
     if filter_words.lower() != 'нет':
+        words = filter_words.split(' ')
         new_list = []
         for vacancy in city_vacancies:
-            for words in filter_words:
+            for word in words:
                 if words in vacancy.description or words in vacancy.requirements:
                     new_list.append(vacancy)
         return new_list
@@ -24,8 +25,11 @@ def get_vacancies_by_salary(filtered_vacancies, salary_range):
         salary = salary_range.split(' ')
         new_list = []
         for vacancy in filtered_vacancies:
-            if salary[0] < vacancy.salary_from < salary[1]:
-                if salary[0] < vacancy.salary_to < salary[1]:
+            if int(salary[0]) <= vacancy.salary_from <= int(salary[1]):
+                if vacancy not in new_list:
+                    new_list.append(vacancy)
+            elif int(salary[0]) <= vacancy.salary_to <= int(salary[1]):
+                if vacancy not in new_list:
                     new_list.append(vacancy)
         return new_list
     return filtered_vacancies
--- src/saver.py
+++ src/saver.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import json
 
 
 class Save(ABC):
@@ -8,15 +9,16 @@ class Save(ABC):
 
 
 class SaveToJSON(Save):
+
     def __init__(self, file_path, data_to_save):
-        self.data_to_save = json.dump(data_to_save)
+        self.data_to_save = data_to_save
         self.file_path = file_path
 
     def save(self):
         with open(self.file_path, 'w', encoding='utf-8') as f:
-            f.write(self.data_to_save)
+            json.dump(self.data_to_save, f)
 
     def show(self):
         with open(self.file_path, 'r', encoding='utf-8') as f:
-            data_to_show = f.read(self.data_to_save)
+            data_to_show: str = f.read(self.data_to_save)
         return json.load(data_to_show)
